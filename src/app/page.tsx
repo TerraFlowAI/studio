c5e88dedf686ed7cb43108cdb77d769a44503611
koushik Umesh <koushikumesha@gmail.com>
2025-06-21 06:02:22 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -419,7 +419,7 @@ export default function LandingPage() {
                           </div>
                           <div className="bg-slate-700 rounded-lg overflow-hidden shadow-[-5px_-5px_10px_rgba(100,116,139,0.25),_5px_5px_10px_rgba(15,23,42,0.35)] relative group">
                              <Image 
-                              src="https://placehold.co/400x250.png"
+                              src="https://images.unsplash.com/photo-1601039913996-d74e58095333?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxMHx8Q29tbWVyY2lhbCUyMHNwYWNlfGVufDB8fHx8MTc1MDQ4NTYxMXww&ixlib=rb-4.1.0&q=80&w=1080"
                               alt="Commercial Space"
                               width={400}
                               height={250}
