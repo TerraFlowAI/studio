13854e2b02f87863bd1e162e876dbd265b43e670
koushik Umesh <koushikumesha@gmail.com>
2025-06-23 03:41:26 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased flex flex-col min-h-screen" suppressHydrationWarning={true}>
+      <body className="font-body antialiased flex flex-col min-h-screen">
         <AuthProvider>
           <ThemeProvider
               attribute="class"
