4892a4f7ec108301da09cbfbdfab5342127982a1
koushik Umesh <koushikumesha@gmail.com>
2025-06-27 11:50:32 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <LinkComponent hre

--- src/components/landing/FeatureShowcase.tsx
+++ src/components/landing/FeatureShowcase.tsx
@@ -159,7 +159,7 @@ const FeatureBlock = ({ block }: { block: typeof featureBlocks[0] }) => {
 
 export function FeatureShowcase() {
   return (
-    <section className="bg-white py-16 md:py-24">
+    <section id="features" className="bg-white py-16 md:py-24">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <motion.div
           initial={{ opacity: 0, y: 20 }}
